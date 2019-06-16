82c2eb3b5d068840fc1b5cccde1f6cc5b90be980
gpskwlkr <giorgianakidze@outlook.com>
2019-06-16 23:43:49 +0400

    bugfix & json api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_api import status
 from config import Config
 from urlGenerator import generateURL
@@ -38,9 +38,22 @@ def shrt(shortID = None):
             So to make everything work perfect we're just checking is it so, or not, before fetching it's URL from database.
         '''
         shortID = int(shortID)
-        return redirect(db.getUrl(shortID))
+        url = db.getUrl(shortID)
+        print(url)
+        if url.startswith('https://', 0, 8) or url.startswith('http://', 0, 7):
+            print('first if')
+            return redirect(url)
+        else:
+            url = f'http://{url}'
+
+        return redirect(url)
     except:
-        return redirect(db.getUrl(shortID))
+        url = db.getUrl(shortID)
+        if url[:4] != 'http':
+            print('second if')
+            url = f'http://{url}'
+        
+        return redirect(url)
 
 
 @app.route('/short', methods=['POST'])
@@ -60,6 +73,22 @@ def short():
         return redirect(url_for('main', url=shortened))
     else:
         return redirect(url_for('main', url=f'{Config.BASE_URL}/{url_exists}'))
+
+
+
+
+@app.route('/api/v1/', methods=['POST'])
+def api(url=None):
+    url: str = request.get_json()['url']
+    print(url)
+    url_exists = db.checkUrl(url)
+    if not url_exists:
+        shortened: str = generateURL()
+        shortened_id: str = shortened.split('/')[2]
+        db.addEntry(shortened_id, url)
+        return jsonify(url = f'http://{shortened}')
+    else:
+        return jsonify(url = f'http://{Config.BASE_URL}/{url_exists}')
 
 
 if __name__ == '__main__':
